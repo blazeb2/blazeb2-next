afb9d6917744a7da96cf01ae619a6c4b30ab82da
ryanuo <1577791638@qq.com>
2024-06-23 20:29:20 +0800

    chore: adjust;

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+
+// eslint-disable-next-line ts/ban-ts-comment
+// @ts-expect-error
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
