5d61597c352aff156e3edc02daa9585eb482aae8
ryanuo <1577791638@qq.com>
2024-06-30 23:12:47 +0800

    feat: add photo preview;

--- components/b2/imageList/masonryWrap.tsx
+++ components/b2/imageList/masonryWrap.tsx
@@ -1,7 +1,15 @@
 'use client'
 
 import PhotoAlbum from 'react-photo-album'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
+import Lightbox from 'yet-another-react-lightbox'
+import 'yet-another-react-lightbox/styles.css'
+
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen'
+import Slideshow from 'yet-another-react-lightbox/plugins/slideshow'
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails'
+import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import 'yet-another-react-lightbox/plugins/thumbnails.css'
 import type { FileUploadInfo } from '@/app/api/list'
 
 const heights = [780, 1620, 720, 607, 608, 1549, 694, 1440, 810, 595, 160]
@@ -10,6 +18,7 @@ interface MasonryWrapProps {
 }
 export function MasonryWrap(props: MasonryWrapProps) {
   const { files } = props
+  const [index, setIndex] = useState(-1)
 
   const ps = useMemo(() => {
     const photos = files?.map((file) => {
@@ -25,18 +34,29 @@ export function MasonryWrap(props: MasonryWrapProps) {
   }, [files])
 
   return (
-    <PhotoAlbum
-      photos={ps}
-      layout="rows"
-      defaultContainerWidth={1650}
-      sizes={{
-        size: 'calc(100vw - 40px)',
-        sizes: [
-          { viewport: '(max-width: 299px)', size: 'calc(100vw - 10px)' },
-          { viewport: '(max-width: 599px)', size: 'calc(100vw - 20px)' },
-          { viewport: '(max-width: 1199px)', size: 'calc(100vw - 30px)' },
-        ],
-      }}
-    />
+    <>
+      <PhotoAlbum
+        photos={ps}
+        layout="rows"
+        defaultContainerWidth={1650}
+        onClick={({ index }) => setIndex(index)}
+        sizes={{
+          size: 'calc(100vw - 40px)',
+          sizes: [
+            { viewport: '(max-width: 299px)', size: 'calc(100vw - 10px)' },
+            { viewport: '(max-width: 599px)', size: 'calc(100vw - 20px)' },
+            { viewport: '(max-width: 1199px)', size: 'calc(100vw - 30px)' },
+          ],
+        }}
+      />
+      <Lightbox
+        slides={ps}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
   )
 }
