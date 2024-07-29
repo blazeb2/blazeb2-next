9cfb90b0de5f0697719a90b23dd0f2976d872035
ryanuo <1577791638@qq.com>
2024-07-29 21:52:47 +0800

    fix: server window is not defined;

--- lib/hooks/useFollowMouse.ts
+++ lib/hooks/useFollowMouse.ts
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Position {
   x: number | string
@@ -6,32 +6,24 @@ interface Position {
 }
 
 export function useFollowMouse() {
-  const [position, setPosition] = useState<Position>({ x: window.innerWidth / 2 - 100, y: 200 })
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 })
   const [dragging, setDragging] = useState<boolean>(false)
   const [offset, setOffset] = useState<Position>({ x: 0, y: 0 })
 
-  const handleMouseDown = (e: any) => {
-    setDragging(true)
-    setOffset({
-      x: e.clientX - (position.x as number),
-      y: e.clientY - (position.y as number),
-    })
-  }
-
-  const handleMouseMove = (e: MouseEvent) => {
-    if (dragging) {
-      setPosition({
-        x: e.clientX - (offset.x as number),
-        y: e.clientY - (offset.y as number),
-      })
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (dragging) {
+        setPosition({
+          x: e.clientX - (offset.x as number),
+          y: e.clientY - (offset.y as number),
+        })
+      }
     }
-  }
 
-  const handleMouseUp = () => {
-    setDragging(false)
-  }
+    const handleMouseUp = () => {
+      setDragging(false)
+    }
 
-  React.useEffect(() => {
     if (dragging) {
       window.addEventListener('mousemove', handleMouseMove)
       window.addEventListener('mouseup', handleMouseUp)
@@ -45,7 +37,19 @@ export function useFollowMouse() {
       window.removeEventListener('mousemove', handleMouseMove)
       window.removeEventListener('mouseup', handleMouseUp)
     }
-  }, [dragging])
+  }, [dragging, offset])
+
+  const handleMouseDown = (e: any) => {
+    setDragging(true)
+    setOffset({
+      x: e.clientX - (position.x as number),
+      y: e.clientY - (position.y as number),
+    })
+  }
+
+  useEffect(() => {
+    setPosition({ x: window.innerWidth / 2 - 100, y: 200 })
+  }, []) // Ensure initial position is set after first render
 
   return {
     position,
